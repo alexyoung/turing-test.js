9a4c33e79cbeb0bf2a719bc155590bdb14c7390b
Alex Young <alex@helicoid.net>
2010-11-11 19:11:30 +0000

    Added most of the assertions as defined by CommonJS

--- test/assertions.js
+++ test/assertions.js
@@ -12,16 +12,44 @@ exports['test ok'] = function() {
   assert.ok(true, 'True should be OK');
 };
 
-exports['test failed assertions'] = function() {
-  assert.ok(false, 'Is not OK');
+exports['test strictEqual'] = function() {
+  assert.strictEqual('1', '1', "'1' should be equal to '1'");
+  assert.strictEqual(1, 1, '1 should be equal to 1');
 };
 
-exports['test exceptions are caught by the reporter'] = function() {
-  throw('Intentionally raised');
+exports['test notStrictEqual'] = function() {
+  assert.notStrictEqual(1, '1');
+  assert.notStrictEqual('1', 1);
 };
 
-exports['test weird things are caught'] = function() {
-  a + 1;
+exports['test deepEqual'] = function() {
+  assert.deepEqual(1, 1);
+  assert.deepEqual('1', '1');
+  assert.deepEqual(new Date(1), new Date(1));
+  assert.deepEqual([1, 2, 3], [1, 2, 3]);
+  assert.deepEqual(
+    [[3, 2, 1], 2, [1, 2, 3]],
+    [[3, 2, 1], 2, [1, 2, 3]]
+  );
+  assert.deepEqual(
+    { name: 'Alex', position: 'Expert Button Pusher' },
+    { name: 'Alex', position: 'Expert Button Pusher' }
+  );
+};
+
+exports['test notDeepEqual'] = function() {
+  assert.notDeepEqual(1, 2);
+  assert.notDeepEqual('1', '2');
+  assert.notDeepEqual(new Date(1), new Date(2));
+  assert.notDeepEqual([1, 2, 3], [3, 2, 1]);
+  assert.notDeepEqual(
+    [[3, 2, 1], 2, [1, 2, 3]],
+    [[3, 2, 1], 2, [1, 2, 1]]
+  );
+  assert.notDeepEqual(
+    { name: 'Alex', position: 'Expert Button Pusher' },
+    { name: 'Mike', position: 'Expert Button Pusher' }
+  );
 };
 
 if (typeof module !== 'undefined')
